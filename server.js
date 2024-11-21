c45dc01c9811aecf6587d62b970c7778d2d3752a
Gabriel Viana <37019532@sempreunijuazeiro.com.br>
2024-11-21 00:07:45 -0300

    adicionado configuração de conexão no banco mongo

--- server.js
+++ server.js
@@ -1,57 +1,9 @@
 import express from "express";
-
-const posts = [
-  {
-    id: 1,
-    descricao: "uma foto teste",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 2,
-    descricao: "Gato tomando sol",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 3,
-    descricao: "Paisagem incrível",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 4,
-    descricao: "Cachorro brincando",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 5,
-    descricao: "Comida deliciosa",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 6,
-    descricao: "Desenho artístico",
-    imagem: "https://placecats.com/millie/300/100/150",
-  },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
   console.log("server listening");
-});
-
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
-});
-
-function buscarPostPorID(id) {
-  return posts.findIndex((post) => {
-    return post.id === Number(id);
-  });
-}
-
-app.get("/posts/:id", (req, res) => {
-  const index = buscarPostPorID(req.params.id);
-  res.status(200).json(posts[index]);
 });
