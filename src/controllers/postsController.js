9c1251ddf7f85fa600cfc005999cd7e2b3bbf4da
Gabriel Viana <37019532@sempreunijuazeiro.com.br>
2024-11-24 20:36:24 -0300

    adicionado suporte para atualização de posts, integração com Google Generative AI e configuração de CORS

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,10 @@
 import fs from "fs";
-import { getTodosPosts, criarPost } from "../models/postsModel.js";
+import {
+  getTodosPosts,
+  criarPost,
+  atualizarPost,
+} from "../models/postsModel.js";
+import gerarDescricaoComGemini from "../services/geminiService.js";
 
 export async function listarPosts(req, res) {
   const posts = await getTodosPosts();
@@ -28,6 +33,27 @@ export async function uploadImagem(req, res) {
     const postCriado = await criarPost(post);
     const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
     fs.renameSync(req.file.path, imagemAtualizada);
+    res.status(201).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({ Erro: "Falha ao criar" });
+  }
+}
+
+export async function atualizarNovoPost(req, res) {
+  const id = req.params.id;
+  const urlImagem = `http://localhost:3000/${id}.png`;
+
+
+  try {
+    const imgBuffer = fs.readFileSync(`uploads/${id}.png`);
+    const descricao = await gerarDescricaoComGemini(imgBuffer);
+    const post = {
+        imgUrl: urlImagem,
+        descricao: descricao,
+        alt: req.body.alt,
+      };
+    const postCriado = await atualizarPost(id, post);
     res.status(201).json(postCriado);
   } catch (erro) {
     console.error(erro.message);
