3c9201786ed8247c5e03497631e772eb3edaf36b
Gabriel Viana <37019532@sempreunijuazeiro.com.br>
2024-11-24 19:31:21 -0300

    adicionado suporte para upload de imagens e criação de posts

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,7 +1,36 @@
-import getTodosPosts from "../models/postsModel.js";
-
+import fs from "fs";
+import { getTodosPosts, criarPost } from "../models/postsModel.js";
 
 export async function listarPosts(req, res) {
-    const posts = await getTodosPosts();
-    res.status(200).json(posts);
+  const posts = await getTodosPosts();
+  res.status(200).json(posts);
+}
+
+export async function postarNovoPost(req, res) {
+  const post = req.body;
+  try {
+    const postCriado = await criarPost(post);
+    res.status(201).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({ Erro: "Falha ao criar" });
+  }
+}
+
+export async function uploadImagem(req, res) {
+  const post = {
+    descricao: "",
+    imgUrl: req.file.originalname,
+    alt: "",
+  };
+
+  try {
+    const postCriado = await criarPost(post);
+    const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+    fs.renameSync(req.file.path, imagemAtualizada);
+    res.status(201).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({ Erro: "Falha ao criar" });
+  }
 }
